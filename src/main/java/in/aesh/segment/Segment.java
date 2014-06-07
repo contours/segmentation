9e82c16c256084ca09f4872f5ef1b621bb537149
Ryan Shaw <ryanshaw@unc.edu>
2014-06-07 16:05:46 -0400

    make Segment a proper value class

--- src/main/java/in/aesh/segment/Segment.java
+++ src/main/java/in/aesh/segment/Segment.java
@@ -13,9 +13,29 @@ public class Segment {
         this.start = start;
         this.length = length;
     }
-    
+
     @Override
-    public String toString() {
+    public final boolean equals(Object o) {
+        if (! (o instanceof Segment)) {
+            return false;
+        }
+        if (o == this) {
+            return true;
+        }
+        Segment s = (Segment) o;
+        return ((this.start == s.start) && (this.length == s.length));
+    }
+
+    @Override
+    public final int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + this.start;
+        hash = 67 * hash + this.length;
+        return hash;
+    }
+
+    @Override
+    public final String toString() {
         return String.format("(%s,%s)", this.start, this.length);
     }
 }
